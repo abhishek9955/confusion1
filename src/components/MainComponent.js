38e0b3f7a2cefffe5502d62a337d3ee6d87e06b3
Abhishek Kumar <abhishekkumar995529@gmail.com>
2021-06-13 06:45:45 +0530

    week 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
+import About from './AboutComponent';
 import Contact from './ContactComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
@@ -36,6 +37,14 @@ class Main extends Component {
       );
     }
 
+    const AboutUsPage = () => {
+      return(
+          <About 
+              leaders={this.state.leaders}
+          />
+      );
+  };
+
     const DishWithId = ({match}) => {
       return(
           <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
@@ -50,7 +59,8 @@ class Main extends Component {
               <Route path='/home' component={HomePage} />
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
               <Route path='/menu/:dishId' component={DishWithId} />
-              <Route exact path='/contactus' component={Contact} />} />
+              <Route exact path='/contactus' component={Contact} />
+              <Route exact path="/aboutus" component={ AboutUsPage } />
               <Redirect to="/home" />
           </Switch>
         <Footer />
