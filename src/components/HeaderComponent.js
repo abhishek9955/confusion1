e63276841c4cd1dd3f9bdc4a33efa4c6c4e9c5be
Abhishek Kumar <abhishekkumar995529@gmail.com>
2021-06-13 19:53:26 +0530

    Intro to redux

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -58,9 +58,8 @@ class Header extends Component {
                                 <NavLink className="nav-link" to='/contactus'><span className="fa fa-address-card fa-lg"></span> Contact Us</NavLink>
                             </NavItem>
                             </Nav>
-                            
                         </Collapse>
-                        <Nav className="ml-auto" navbar>
+                            <Nav className="ml-auto" navbar>
                                 <NavItem>
                                     <Button outline onClick={this.toggleModal}><span className="fa fa-sign-in fa-lg"></span> Login</Button>
                                 </NavItem>
